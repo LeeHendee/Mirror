f822d5609746713e58ca603b9431f28115f1c8aa
梁再盼 <1175421985@qq.com>
2016-03-31 09:50:11 +0800

    三方登录～

---
+++ app/src/main/java/com/my/mirror/lzp/ForgetPasswordActivity.java
@@ -0,0 +1,24 @@
+package com.my.mirror.lzp;
+
+import com.my.mirror.R;
+import com.my.mirror.base.BaseActivity;
+
+/**
+ * Created by liangzaipan on 16/3/29.
+ */
+public class ForgetPasswordActivity extends BaseActivity {
+    @Override
+    protected int getLayout() {
+        return R.layout.activity_forgetpassword;
+    }
+
+    @Override
+    protected void initData() {
+
+    }
+
+    @Override
+    protected void initView() {
+
+    }
+}
