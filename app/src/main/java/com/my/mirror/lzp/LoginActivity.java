99151e3245245743ec497b7f8e1b6cfc16d39097
梁再盼 <1175421985@qq.com>
2016-03-31 10:57:10 +0800

    no message

--- app/src/main/java/com/my/mirror/lzp/LoginActivity.java
+++ app/src/main/java/com/my/mirror/lzp/LoginActivity.java
@@ -12,14 +12,6 @@ import android.widget.TextView;
 import com.my.mirror.R;
 import com.my.mirror.base.BaseActivity;
 
-import java.util.HashMap;
-
-import cn.sharesdk.framework.Platform;
-import cn.sharesdk.framework.PlatformActionListener;
-import cn.sharesdk.framework.ShareSDK;
-import cn.sharesdk.sina.weibo.SinaWeibo;
-import cn.sharesdk.wechat.friends.Wechat;
-
 /**
  * Created by liangzaipan on 16/3/29.
  */
@@ -50,7 +42,7 @@ public class LoginActivity extends BaseActivity implements View.OnClickListener 
 
     @Override
     protected void initView() {
-        //ShareSDK.initSDK(this);
+        ShareSDK.initSDK(this);
         phoneEt = (EditText) findViewById(R.id.login_phoneNum_edit);
         passwordEt = (EditText) findViewById(R.id.login_password_edit);
         loginBtn = (Button) findViewById(R.id.login_btn);
@@ -74,55 +66,55 @@ public class LoginActivity extends BaseActivity implements View.OnClickListener 
                 finish();
                 break;
             case R.id.xinlang_icon:
-//                ShareSDK.initSDK(this);
-//                Platform platformXl = ShareSDK.getPlatform(SinaWeibo.NAME);
-//                if (platformXl.isAuthValid()) {
-//                    platformXl.removeAccount();
-//                }
-//                platformXl.setPlatformActionListener(new PlatformActionListener() {
-//                    @Override
-//                    public void onComplete(Platform platform, int i, HashMap<String, Object> hashMap) {
-//
-//                    }
-//
-//                    @Override
-//                    public void onError(Platform platform, int i, Throwable throwable) {
-//
-//                    }
-//
-//                    @Override
-//                    public void onCancel(Platform platform, int i) {
-//
-//                    }
-//                });
-//                platformXl.SSOSetting(false);
-//                platformXl.showUser(null);
-//                break;
-//            case R.id.weixin_icon:
-//                ShareSDK.initSDK(this);
-//                Platform platformWx = ShareSDK.getPlatform(Wechat.NAME);
-//                if (platformWx.isAuthValid()) {
-//                    platformWx.removeAccount();
-//                }
-//                platformWx.setPlatformActionListener(new PlatformActionListener() {
-//                    @Override
-//                    public void onComplete(Platform platform, int i, HashMap<String, Object> hashMap) {
-//
-//                    }
-//
-//                    @Override
-//                    public void onError(Platform platform, int i, Throwable throwable) {
-//
-//                    }
-//
-//                    @Override
-//                    public void onCancel(Platform platform, int i) {
-//
-//                    }
-//                });
-//                platformWx.SSOSetting(false);
-//                platformWx.showUser(null);
-//                break;
+                ShareSDK.initSDK(this);
+                Platform platformXl = ShareSDK.getPlatform(SinaWeibo.NAME);
+                if (platformXl.isAuthValid()) {
+                    platformXl.removeAccount();
+                }
+                platformXl.setPlatformActionListener(new PlatformActionListener() {
+                    @Override
+                    public void onComplete(Platform platform, int i, HashMap<String, Object> hashMap) {
+
+                    }
+
+                    @Override
+                    public void onError(Platform platform, int i, Throwable throwable) {
+
+                    }
+
+                    @Override
+                    public void onCancel(Platform platform, int i) {
+
+                    }
+                });
+                platformXl.SSOSetting(false);
+                platformXl.showUser(null);
+                break;
+            case R.id.weixin_icon:
+                ShareSDK.initSDK(this);
+                Platform platformWx = ShareSDK.getPlatform(Wechat.NAME);
+                if (platformWx.isAuthValid()) {
+                    platformWx.removeAccount();
+                }
+                platformWx.setPlatformActionListener(new PlatformActionListener() {
+                    @Override
+                    public void onComplete(Platform platform, int i, HashMap<String, Object> hashMap) {
+
+                    }
+
+                    @Override
+                    public void onError(Platform platform, int i, Throwable throwable) {
+
+                    }
+
+                    @Override
+                    public void onCancel(Platform platform, int i) {
+
+                    }
+                });
+                platformWx.SSOSetting(false);
+                platformWx.showUser(null);
+                break;
             case R.id.login_forget:
                 Intent intent1 = new Intent(this,ForgetPasswordActivity.class);
                 startActivity(intent1);
