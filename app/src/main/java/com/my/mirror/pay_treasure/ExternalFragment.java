a631db821356fd42fd30fcfa7543d4d35ad0fc1b
田荣闯 <1129440815@qq.com>
2016-04-18 16:05:21 +0800

    no message

---
+++ app/src/main/java/com/my/mirror/pay_treasure/ExternalFragment.java
@@ -0,0 +1,20 @@
+package com.my.mirror.pay_treasure;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.my.mirror.R;
+
+/**
+ * Created by TRC on 16/4/7.
+ */
+public class ExternalFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.pay_external, container, false);
+    }
+}
